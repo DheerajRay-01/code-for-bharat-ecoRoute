4c659c50d3cdc68992b37fe2729f6c7ce6a9093e
DheerajRay-01 <dheerajray2003@gmail.com>
2025-07-13 00:23:14 +0530

    correct axios

--- server/controller/auth.controller.js
+++ server/controller/auth.controller.js
@@ -15,6 +15,8 @@ const googleAuth = asyncHandler(async (req, res) => {
 
   const accessToken = generateAccessToken({ _id, fullName, email });
   const refreshToken = generateRefreshToken({ _id, email });
+  console.log("accessToken",accessToken);
+  
 
   if (!accessToken || !refreshToken) {
     throw new ApiError(400, "Tokens not generated");

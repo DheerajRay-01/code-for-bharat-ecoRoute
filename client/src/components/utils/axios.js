32ddd800ad6d23fa2329767534f0ec4ef70e6fee
DheerajRay-01 <dheerajray2003@gmail.com>
2025-07-13 01:34:36 +0530

    fix axios file

--- client/src/components/utils/axios.js
+++ client/src/components/utils/axios.js
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-//   baseURL: import.meta.env.VITE_API_URL, // ✅ use env for production
-  baseURL: "/api", // ✅ use env for production
+  baseURL: import.meta.env.VITE_API_URL, // ✅ use env for production
   withCredentials: true, // ✅ important for cookies
 });
 

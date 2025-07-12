8784b8ec47b3d6efcf22c01e847f785037f3c3a2
DheerajRay-01 <dheerajray2003@gmail.com>
2025-07-13 01:18:27 +0530

    correct login url in login

--- client/src/components/utils/axios.js
+++ client/src/components/utils/axios.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL:'/api',
-    withCredentials: true, 
-})
+  baseURL: import.meta.env.VITE_API_URL, // ✅ use env for production
+  withCredentials: true, // ✅ important for cookies
+});
 
-export default axiosInstance
+export default axiosInstance;
